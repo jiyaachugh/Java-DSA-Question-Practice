797d77aaabf00d22ceadf842e5658f04f7150410
jiya chugh <jiyaachugh@gmail.com>
2024-07-28 23:58:20 +0530

    Basic Recursion questions

---
+++ Basic Recursion/PrintNumbers.java
@@ -0,0 +1,14 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        printNo(5);
+    }
+
+    public static void printNo(int n) {
+        if(n == 1 ) { System.out.print(1);
+        return;}
+        printNo(n-1);
+        System.out.print(n);
+
+
+    }
+}
