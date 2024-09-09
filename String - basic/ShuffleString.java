361c953a75230110ae9592af2d7c7dcdb4055650
jiya chugh <jiyaachugh@gmail.com>
2024-09-09 22:01:59 +0530

    DSA questions

---
+++ String - basic/ShuffleString.java
@@ -0,0 +1,9 @@
+public class ShuffleString {
+    public static void main(String[] args) {
+
+    }
+
+    public static int shuffleArr() {
+        
+    }
+}
---
+++ String - basic/TwoSumBS.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class TwoSumBS {
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+//        twoSumBS(numbers, target);
+    }
+
+//    public static String twoSumBS(int[] numbers, int target) {
+//        int start = 0;
+//        int last = numbers.length - 1;
+//        int[] arr = new int[2];
+//        while(numbers[start] != numbers[last]) {
+//
+//        }
+//    }
+}
