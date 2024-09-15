2559f5802c764dc6e8acc379d18ad27fd6cbcdf5
jiya chugh <jiyaachugh@gmail.com>
2024-09-16 00:01:54 +0530

    recursion ques

---
+++ Recursion/print1toN.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class print1toN {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int N = in.nextInt();
+        printNos(N);
+
+    }
+
+    public static void printNos(int N) {
+        if(N == 0) {
+            return;
+        }
+        System.out.println(N);
+        printNos(N - 1);
+    }
+}
