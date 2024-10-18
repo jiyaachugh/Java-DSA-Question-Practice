2def1e342f649124d88632ca27ddddb6ed5a75de
jiya chugh <jiyaachugh@gmail.com>
2024-10-18 23:56:02 +0530

    LinkedList ques

---
+++ LinkedList/src/AddingElements.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class AddingElements {
+    public static void main(String[] args) {
+
+                LinkedList<String> list = new LinkedList<String>();
+
+
+                list.add("One");
+                list.add("Two");
+                list.add("Three");
+                list.add("Four");
+                list.add("Five");
+
+                System.out.println(list);
+
+    }
+}
