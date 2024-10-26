6496598ebd25f7ce72f3aa1b79d9b068f1b30cd8
jiya chugh <jiyaachugh@gmail.com>
2024-10-26 21:22:13 +0530

    String DSA ques

---
+++ String - Easy/FirstOccur.java
@@ -0,0 +1,10 @@
+public class FirstOccur {
+    public static void main(String[] args) {
+        String haystack = "jiyachughji";
+        String needle = "ji";
+    }
+    static int firstOcc(String haystack,String needle) {
+        
+
+    }
+}
